5fb86396e0073f51c11b8a977edc51dbde2719e2
Trey Marak <treymarak@alfreds-mbp.attlocal.net>
2018-09-12 22:52:28 -0500

    work on html

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mySql = require("mysql");
-var handlebars = require("express-handlebars");
+var exphbs = require("express-handlebars");
